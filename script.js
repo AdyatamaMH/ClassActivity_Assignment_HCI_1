fb2bf659fac374ed0bce68ca4980b402ee2c5642
AdyatamaMH <114073452+AdyatamaMH@users.noreply.github.com>
2022-12-18 21:07:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //Please put your answer here
+$('.fa-times').click(
+    function()
+{
+    $('.signup-modal-wrapper').fadeToggle(100);
+    
+});
+
+$('.btn').click(
+    function()
+{
+    $('.signup-modal-wrapper').fadeToggle(100);
+
+});
+
+$('.btn').click(
+    function()
+{
+    $('.signup-modal-wrapper').fadeIn(1000);
+
+});
